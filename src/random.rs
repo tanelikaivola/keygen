dbf19db674752d70d37e5c44417feb785a830baa
Taneli Kaivola <tanelikaivola@users.noreply.github.com>
2024-03-03 15:49:14 +0200

    shorten u64<->[u8] conversions

--- src/random.rs
+++ src/random.rs
@@ -13,30 +13,12 @@ use tiny_keccak::Hasher;
 use tiny_keccak::Sha3;
 use zeroize::Zeroize;
 
-fn u64_to_bytes(val: u64) -> [u8; 8] {
-    [
-        (val >> 56) as u8,
-        (val >> 48) as u8,
-        (val >> 40) as u8,
-        (val >> 32) as u8,
-        (val >> 24) as u8,
-        (val >> 16) as u8,
-        (val >> 8) as u8,
-        val as u8,
-    ]
+const fn u64_to_bytes(val: u64) -> [u8; 8] {
+    val.to_be_bytes()
 }
 
 fn vec_u8_to_u64(bytes: &[u8]) -> Option<u64> {
-    if bytes.len() != 8 {
-        return None; // Ensure that the input has exactly 8 bytes
-    }
-
-    let mut result: u64 = 0;
-    for &byte in bytes {
-        result = (result << 8) | u64::from(byte);
-    }
-
-    Some(result)
+    bytes.try_into().map(u64::from_be_bytes).ok()
 }
 
 struct BitVector {
