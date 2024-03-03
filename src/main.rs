bb0320be329271b74129449bcc5672c3341e394e
Taneli Kaivola <tanelikaivola@users.noreply.github.com>
2024-03-03 15:57:13 +0200

    move print_formatted_value to numformat

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use alphabet::{
 use zeroize::Zeroize;
 
 mod numformat;
-use numformat::NumFormat;
+use numformat::{print_formatted_value, NumFormat};
 
 mod randomsource;
 use randomsource::RandomSource;
@@ -35,50 +35,6 @@ struct Config {
     delimiter: String,
     count: usize,
     rngtest: Option<(RandomSource, u32, NumFormat)>,
-}
-
-fn print_formatted_value(value: u64, mode: NumFormat) {
-    match mode {
-        NumFormat::RawBinary => {
-            for shift in (0..=56).step_by(8) {
-                let byte = ((value >> shift) & 0xFF) as u8;
-                print!("{}", byte as char);
-            }
-        }
-        NumFormat::U8 => {
-            let bytes: [u8; 8] = value.to_be_bytes();
-            for byte in &bytes {
-                println!("{}", byte);
-            }
-        }
-        NumFormat::U16 => {
-            let bytes: [u8; 8] = value.to_be_bytes();
-            let u16_values: [u16; 4] = [
-                u16::from_be_bytes([bytes[0], bytes[1]]),
-                u16::from_be_bytes([bytes[2], bytes[3]]),
-                u16::from_be_bytes([bytes[4], bytes[5]]),
-                u16::from_be_bytes([bytes[6], bytes[7]]),
-            ];
-            for u16_value in &u16_values {
-                println!("{}", u16_value);
-            }
-        }
-        NumFormat::U32 => {
-            let bytes: [u8; 8] = value.to_be_bytes();
-            let u32_values: [u32; 2] = [
-                u32::from_be_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]),
-                u32::from_be_bytes([bytes[4], bytes[5], bytes[6], bytes[7]]),
-            ];
-            for u32_value in &u32_values {
-                println!("{}", u32_value);
-            }
-        }
-        NumFormat::U64 => {
-            let bytes: [u8; 8] = value.to_be_bytes();
-            let u64_value = u64::from_be_bytes(bytes);
-            println!("{}", u64_value);
-        }
-    }
 }
 
 fn main() {
